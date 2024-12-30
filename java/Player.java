929c40287dbcf1f548a511ead564b4615b78b746
Taleeha Tahoor <taleeha789@gmail.com>
2024-12-30 19:23:24 +0500

    Implemented player abstract class, Human Player, Computer Player, sub classes and Main class

--- java/Player.java
+++ java/Player.java
@@ -1,3 +1,13 @@
 public class Player {
-    
+    protected String symbol;
+
+    public Player(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public abstract void makeMove(Board board);
 }
